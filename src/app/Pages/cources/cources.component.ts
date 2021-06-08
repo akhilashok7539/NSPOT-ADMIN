bfd3380f25109cd5856c6acfeaf60f9f78f0febf
Akhil Ashok <akhil.ashok@accubits.com>
2021-06-08 17:24:47 +0530

    chanegs

--- src/app/Pages/cources/cources.component.ts
+++ src/app/Pages/cources/cources.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-cources',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cources.component.css']
 })
 export class CourcesComponent implements OnInit {
-  bankItems:any=[];
-  constructor() { }
+  courses:any=[];
+  constructor(private apiserive:ApiService) { }
 
   ngOnInit(): void {
+    this.getallcourser();
   }
 
+  getallcourser(){
+    let req= 
+    {
+
+    }
+    this.apiserive.doPostRequest(`/institute/course/filter`,req).subscribe(
+      data =>{
+        this.courses = data['data']
+      }
+    )
+  }
 }
